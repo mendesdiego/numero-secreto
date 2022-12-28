e542d629f944cbf1825c4812c50c8dd283c87732
Diego Mendes <mendes87.diego@gmail.com>
2022-12-28 16:20:58 -0300

    Update sortearNumero.js

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -6,7 +6,5 @@ function gerarNumeroAleatorio() {
    return parseInt(Math.random() * maiorValor + 1)
 }
 
-console.log('Número secreto: ', numeroSecreto);
-
 const elementoMenorValor = document.getElementById('menor-valor').innerHTML = menorValor
 const elementoMaiorValor = document.getElementById('maior-valor').innerHTML = maiorValor
